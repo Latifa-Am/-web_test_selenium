4a862cdc45acab36dcb63262656a4292088bddcd
Latifa_am <105515930+Latifa-Am@users.noreply.github.com>
2023-02-20 12:17:59 +0100

    create python test fixture

--- tests/test_web.py
+++ tests/test_web.py
@@ -1,2 +1,15 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait as wait
+
+url = "https://www.mozilla.org"
+title  = "Internet for people"
+
+@pytest.fixture
+def webFix():
+    driver = webdriver.Firefox()
+    driver.get(url)
+    
 def test_add():
     assert 4 + 2 == 6
